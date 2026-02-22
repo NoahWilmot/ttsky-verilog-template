9cbe106311b59ed771050ca2bd4fab1254baf808
Noah Wilmot <flashw22@gmail.com>
2026-02-22 11:45:48 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -19,13 +19,16 @@ module tt_um_example (
   logic reset;
   assign reset = ~rst_n;
     
-    RangeFinder #(8) RF(.data_in(ui_in), .clock(clk),. reset(reset), .go(uio_in[0]), .finish(uio_in[1]), .range(uo_out), .error(uio_out[2]));
+  RangeFinder #(8) RF(.data_in(ui_in), .clock(clk),. reset(reset), .go(uio_in[0]), .finish(uio_in[1]), .range(uo_out), .error(uio_out[2]));
 
   // All output pins must be assigned. If not used, assign to 0.
   //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_oe  = 8'b0010_0000;
 
+  assign uio_out[1:0] = 2'b00;
+  assign uio_out[7:3] = 5'b00000;
+
   // List all unused inputs to prevent warnings
-    wire _unused = &{ena, uio_in[3], uio_in[4], uio_in[5], uio_in[6], uio_in[7], 1'b0};
+  wire _unused = &{ena, uio_in[2], uio_in[3], uio_in[4], uio_in[5], uio_in[6], uio_in[7], 1'b0};
 
 endmodule
