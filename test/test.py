6d893b6141edf927eabb618e9b0bac98c9d0e8f5
Noah Wilmot <flashw22@gmail.com>
2026-04-30 15:31:16 -0400

    Add files via upload

--- test/test.py
+++ test/test.py
@@ -5,8 +5,7 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
-@cocotb.test()
-async def test_basic_sweep(dut):
+async def init(dut):
     cocotb.start_soon(Clock(dut.clk, 40, unit="ns").start())
     dut.ui_in.value  = 0
     dut.uio_in.value = 0
@@ -14,15 +13,19 @@ async def test_basic_sweep(dut):
     dut.rst_n.value  = 0
     await ClockCycles(dut.clk, 5)
     dut.rst_n.value  = 1
-    await ClockCycles(dut.clk, 10)
+    await ClockCycles(dut.clk, 5)
+
+@cocotb.test()
+async def test_basic_sweep(dut):
+    await init(dut)
     assert True
 
 @cocotb.test()
 async def test_different_patterns(dut):
-    await ClockCycles(dut.clk, 1)
+    await init(dut)
     assert True
 
 @cocotb.test()
 async def test_wr_en_gated(dut):
-    await ClockCycles(dut.clk, 1)
+    await init(dut)
     assert True
