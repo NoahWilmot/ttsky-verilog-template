85ba42414cb875bc3cf4151ed6dc125a74d9b1d7
Noah Wilmot <flashw22@gmail.com>
2026-04-30 15:16:37 -0400

    Add files via upload

--- src/StimulusGen.sv
+++ src/StimulusGen.sv
@@ -15,11 +15,14 @@ module StimulusGen (
     // Debounce gen
     // -------------------------------------------------------------------------
     localparam int CLK_MHZ = 25;
+    /*
     `ifdef SIM
         localparam int STABLE_MS = 1;
     `else
         localparam int STABLE_MS = 20;
     `endif
+    */
+    localparam int STABLE_MS = 1;
     localparam int STABLE_CNT = CLK_MHZ * 1000 * STABLE_MS;
 
     localparam int DEB_W = $clog2(STABLE_CNT + 1);
