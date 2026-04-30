a165df191345626a208cc3db085c82bd61d8b475
Noah Wilmot <flashw22@gmail.com>
2026-04-30 15:23:49 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -2,31 +2,46 @@
  * Copyright (c) 2024 Your Name
  * SPDX-License-Identifier: Apache-2.0
  */
-
 `default_nettype none
 
 module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
 );
 
-  StimulusGen SG(.clock(clk), .reset_n(rst_n), .gen(ui_in[0]), .wants_ctrl(uio_out[0]), .wr_en(uio_out[1]), .wr_data(uio_out[3:2]), .wr_row(uo_out[3:0]), .wr_col(uo_out[7:4]));
-  //TileGrowth TG(.clock(clk), .reset_n(rst_n), .up(ui_in[0]), .down(ui_in[1]), .left(ui_in[2]), .right(ui_in[3]), .color_sel(ui_in[4]), .place(ui_in[5]), .start(ui_in[6]), .data(uo_out[0]), .count(uio_out));
-  //RangeFinder #(8) RF(.data_in(ui_in), .clock(clk),. reset(reset), .go(uio_in[0]), .finish(uio_in[1]), .range(uo_out), .error(uio_out[2]));
+    wire        wants_ctrl_w;
+    wire        wr_en_w;
+    wire [1:0]  wr_data_w;
+    wire [3:0]  wr_row_w;
+    wire [3:0]  wr_col_w;
 
-  // All output pins must be assigned. If not used, assign to 0.
-  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_oe  = 8'b0000_1111;
-  //assign uo_out[7:1] = 7'b000_0000;
-  assign uio_out[7:4] = 4'b0000;
+    StimulusGen SG (
+        .clock      (clk),
+        .reset_n    (rst_n),
+        .gen        (ui_in[0]),
+        .wants_ctrl (wants_ctrl_w),
+        .wr_en      (wr_en_w),
+        .wr_data    (wr_data_w),
+        .wr_row     (wr_row_w),
+        .wr_col     (wr_col_w)
+    );
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, uio_in,  ui_in[7:1], 1'b0};
+    assign uio_out[0]   = wants_ctrl_w;
+    assign uio_out[1]   = wr_en_w;
+    assign uio_out[3:2] = wr_data_w;
+    assign uio_out[7:4] = 4'b0000;
+
+    assign uo_out[3:0]  = wr_row_w;
+    assign uo_out[7:4]  = wr_col_w;
+
+    assign uio_oe = 8'b0000_1111;
+
+    wire _unused = &{ena, uio_in, ui_in[7:1], 1'b0};
 
 endmodule
