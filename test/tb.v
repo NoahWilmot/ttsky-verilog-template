1e3021002482b730e32ae1bf2cdf25688e51133f
Noah Wilmot <flashw22@gmail.com>
2026-04-30 12:55:30 -0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,49 +1,57 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
+/* Testbench for tt_um_example (StimulusGen)
+   Driven by cocotb test.py
 */
 module tb ();
 
-  // Dump the signals to a FST file. You can view it with gtkwave or surfer.
-  initial begin
-    $dumpfile("tb.fst");
-    $dumpvars(0, tb);
-    #1;
-  end
+    // Dump signals to FST
+    initial begin
+        $dumpfile("tb.fst");
+        $dumpvars(0, tb);
+        #1;
+    end
 
-  // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
+    // Inputs and outputs
+    reg        clk;
+    reg        rst_n;
+    reg        ena;
+    reg  [7:0] ui_in;
+    reg  [7:0] uio_in;
+    wire [7:0] uo_out;
+    wire [7:0] uio_out;
+    wire [7:0] uio_oe;
+
 `ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
+    wire VPWR = 1'b1;
+    wire VGND = 1'b0;
 `endif
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
-
-      // Include power ports for the Gate Level test:
+    tt_um_example user_project (
 `ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
+        .VPWR    (VPWR),
+        .VGND    (VGND),
 `endif
+        .ui_in   (ui_in),
+        .uo_out  (uo_out),
+        .uio_in  (uio_in),
+        .uio_out (uio_out),
+        .uio_oe  (uio_oe),
+        .ena     (ena),
+        .clk     (clk),
+        .rst_n   (rst_n)
+    );
 
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
-  );
+    // Convenience aliases matching StimulusGen port names
+    // Inputs
+    wire gen         = ui_in[0];
+
+    // Outputs
+    wire wants_ctrl  = uio_out[0];
+    wire wr_en       = uio_out[1];
+    wire [1:0] wr_data = uio_out[3:2];
+    wire [3:0] wr_row  = uo_out[3:0];
+    wire [3:0] wr_col  = uo_out[7:4];
 
 endmodule
